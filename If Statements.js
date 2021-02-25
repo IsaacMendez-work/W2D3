3f6ef07f87d3c9779874826b829a5bee036d891c
Isaac Mendez <73685690+IsaacMendez-work@users.noreply.github.com>
2021-02-24 20:29:29 -0500

    Add files via upload

---
+++ If Statements.js
@@ -0,0 +1,17 @@
+for (let cups = 0; cups < 5; cups++)
+{
+    if (cups == 0) {
+        console.log("Yes. I'd love a cup of Joe!");
+    } else if (cups < 3) {
+        console.log("I've only had " + cups + " cups, so I'd love more!");
+    } else {
+        console.log("I think I'm okay for now.");
+    }
+}
+{
+    if (miles > 500) {
+        console.log("I need an oil change.");
+    } else {
+        console.log("No oil change yet.");
+    }
+}
